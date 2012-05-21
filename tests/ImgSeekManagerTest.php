ab945c0e6a618ecc48d7a8d78e1018fb30275ab6
IamPersistent <develop@zestic.com>
2012-05-21 01:53:32 -0700

    add code for tests

---
+++ tests/ImgSeekManagerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace ImgSeek;
+
+use ImgSeek\ImgSeekManager;
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+class ImgSeekManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
+
