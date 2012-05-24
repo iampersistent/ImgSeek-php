661b2e447c22a0fc52ec986e589397ab8de0708e
IamPersistent <develop@zestic.com>
2012-05-23 23:35:54 -0700

    add tests to ImgSeekTestGateway

--- tests/fixtures/ImgSeekTestGateway.php
+++ tests/fixtures/ImgSeekTestGateway.php
@@ -1,13 +1,26 @@
 <?php
 
+use ImgSeek\Exception\ImgSeekException;
 use ImgSeek\Gateway\ImgSeekGatewayInterface;
 /**
  * @author Richard Shank <develop@zestic.com>
  */
 class ImgSeekTestGateway implements ImgSeekGatewayInterface
 {
-    function request($function, array $parameters = null)
+    public function request($function, array $parameters = null)
     {
-        throw new Exception($function);
+        if ($function == 'addImgBlob') {
+            if (!$parameters[0]) {
+                throw new Exception('the first parameter in addImgBlob should have a dbId');
+            }
+            if (!$parameters[1]) {
+                throw new Exception('the second parameter in addImgBlob should have a imageId');
+            }
+            if (!$parameters[2] || is_string($parameters[2])) {
+                throw new Exception('the third parameter in addImgBlob should have a string of image data');
+            }
+
+        }
+        throw new ImgSeekException($function);
     }
 }
